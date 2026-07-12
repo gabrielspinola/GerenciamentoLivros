af957afcb1f69607b94298e44a2e93359eaa0ed0
Gabriel Spinola <gspinola@gmail.com>
2026-07-12 09:17:10 -0300

    Gravando novo ajustes.

--- backend/controllers/auth_controller.py
+++ backend/controllers/auth_controller.py
@@ -5,6 +5,7 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
+
 from config.settings import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
